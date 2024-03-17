b6a179b1a455fad547c3f61195ee3f072ed95810
Дилюк <@DESKTOP-5SJPVS1>
2024-03-17 15:09:10 +0300

    Добавьте файлы проекта.

---
+++ pr7-2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+
+        string inputFile = "F.txt";
+        if (!File.Exists(inputFile))
+        {
+            File.Create(inputFile).Dispose();
+        }
+
+        StreamWriter writer = new StreamWriter(inputFile);
+        writer.WriteLine(31.0);
+        writer.WriteLine(5.0);
+        writer.WriteLine(10.0);
+        writer.WriteLine(12.0);
+        writer.WriteLine(15.0);
+        writer.WriteLine(18.0);
+        writer.WriteLine(20.0);
+        writer.WriteLine(18.0);
+        writer.WriteLine(15.0);
+        writer.WriteLine(12.0);
+        writer.WriteLine(10.0);
+        writer.WriteLine(7.0);
+        writer.Close();
+
+        List<string> lines = File.ReadLines(inputFile).ToList();
+
+        double sum = 0;
+        int monthCount = 0;
+        foreach (string line in lines)
+        {
+            double temp = Convert.ToDouble(line);
+            sum += temp;
+            monthCount++;
+        }
+        double annualAverage = sum / monthCount;
+
+        string outputFile = "G.txt";
+        if (!File.Exists(outputFile))
+        {
+            File.Create(outputFile).Dispose();
+        }
+
+        monthCount = 0;
+        double deviation;
+        foreach (string line in lines)
+        {
+            double temp = Convert.ToDouble(line);
+            deviation = temp - annualAverage;
+            File.AppendAllText(outputFile, $"Month {monthCount + 1}: {deviation}\n");
+            monthCount++;
+        }
+    }
+}
---
+++ pr7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = new int[10];
+
+        Console.WriteLine("Введите 10 чисел:");
+        for (int i = 0; i < 10; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        File.WriteAllLines("numbers.txt", Array.ConvertAll(numbers, x => x.ToString()));
+
+        string[] lines = File.ReadAllLines("numbers.txt");
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        int lastNumber = numbers[numbers.Length - 1] + 1;
+        File.AppendAllText("numbers.txt", Environment.NewLine + lastNumber.ToString());
+
+        Console.WriteLine("Последнее число в массиве: " + lastNumber);
+    }
+}
